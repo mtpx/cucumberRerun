5994200965a850d3a196025ca01869dc8ee9911d
Mateusz Krasny <matpanx@gmail.com>
2019-10-07 19:54:57 +0200

    new

---
+++ src/test/java/opi/steps/YpplicationSteps.java
@@ -0,0 +1,59 @@
+package opi.steps;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import lombok.Getter;
+import lombok.Setter;
+import org.openqa.selenium.By;
+
+import java.util.Random;
+
+import static com.codeborne.selenide.Selenide.$;
+
+
+@Getter
+@Setter
+
+public class YpplicationSteps {
+
+    SelenideElement element = $(By.xpath("//a[contains(text(),'Gmail')]"));
+    SelenideElement element2 = $(By.xpath("//a[contains(text(),'Gmail')]"));
+    SelenideElement element_not_ex = $(By.xpath("//a[contains(text(),'sdfsdfail')]"));
+
+
+
+    @Given("^YOpen google site$")
+    public void openGoogleSite(){
+        Selenide.open("https://www.google.pl");
+    }
+
+    @When("^YUser click not existing button$")
+    public void userClickNotExistingButton(){
+
+       Random r = new Random();
+       int random = r.nextInt(11);
+       if (random<4)
+           element_not_ex.click();
+       else if (random>7)
+           element.click();
+       else
+           element2.click();
+
+
+    }
+
+    @When("^YUser click existing button$")
+    public void userClickExistingButton() throws InterruptedException {
+        Random r = new Random();
+        int random = r.nextInt(11);
+        if (random<4)
+            element_not_ex.click();
+        else if (random>7)
+            element.click();
+        else
+            element2.click();
+
+    }
+
+}
---
+++ src/test/resources/features/Ypplication.feature
@@ -0,0 +1,9 @@
+@application
+Feature: YApplication submit featuresZ
+
+  Scenario: YClick not existing button
+    Given YOpen google site
+    When YUser click existing button
+
+
+
